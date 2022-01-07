aee42e4e05efe8a154922b628bea5528072f56fe
mxyr <mxyr007@126.com>
2022-01-07 17:35:21 +0800

    新加自提点与回到顶部（未完成)

--- miniprogram/my_library/index.js
+++ miniprogram/my_library/index.js
@@ -1,17 +1,35 @@
 function jumpPage(e) {
-    // console.log(`/pages/${e.currentTarget.dataset.jump}/index?param=${e.currentTarget.dataset.param}`);
-    wx.navigateTo({
-      url: `/pages/${e.currentTarget.dataset.jump}/index?param=${e.currentTarget.dataset.param}`,
-    });
+  // console.log(`/pages/${e.currentTarget.dataset.jump}/index?param=${e.currentTarget.dataset.param}`);
+  wx.navigateTo({
+    url: `/pages/${e.currentTarget.dataset.jump}/index?param=${e.currentTarget.dataset.param}`,
+  });
 }
-function copyText(text){
+function copyText(text) {
   wx.setClipboardData({
-    data:text,
+    data: text,
   })
 }
 
+function prefectures_function() {
+  let prefecture_names = ["北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県",
+    "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県",
+    "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県",
+    "静岡県", "愛知県", "三重県", "滋賀県", "京都府", "大阪府", "兵庫県",
+    "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県", "広島県", "山口県",
+    "徳島県", "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県",
+    "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"];
+  let prefectures = {};
+  for (let p of prefecture_names) {
+    prefectures[p] = []
+  }
+  let output = { prefecture_names: prefecture_names, prefectures: prefectures };
+  console.log(output);
+  return output;
+}
 
 
 
-module.exports. jumpPage =  jumpPage;
-module.exports. copyText =  copyText;
+
+module.exports.jumpPage = jumpPage;
+module.exports.copyText = copyText;
+module.exports.prefectures_function = prefectures_function;
--- miniprogram/pages/product/index.js
+++ miniprogram/pages/product/index.js
@@ -1,115 +1,156 @@
 // pages/product/index.js
+var my_library = require('../../my_library/index.js');
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        product:null,
-        sku_length:0,
-        subtitle:"",
-
-        type_index:0,
-    },
-    tag_clicked(e){
-        // console.log(e.currentTarget.dataset.index);
-        this.setData({
-            type_index:e.currentTarget.dataset.index
-        })
-    },
-    getProduct() {
-        wx.showLoading({
-          title: '',
-        });
-       wx.cloud.callFunction({
-          name: 'quickstartFunctions',
-          data: {
-            type: 'getProduct',
-            id:""
-          }
-        }).then((resp) => {
-          console.log(resp);
-          this.setData({
-            product: resp.result.data,
-            sku_length:resp.result.data.sku.length,
-            subtitle:resp.result.data.text.subtitle.split('&hc').join('\n')
-          });
-          // console.log(this.data);
-         wx.hideLoading();
-       }).catch((e) => {
-          console.log(e);
-          wx.hideLoading();
-       });
-      },
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-        this.getProduct();
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-        return {
-            title: this.data.product.text.name,
-            // path: '/pages/index/index',
-            }
-    },
-    setPickUpSpot(e){
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    product: null,
+    sku_length: 0,
+    subtitle: "",
+    show: false,
+    prefecture_names: null,
+    prefectures: null,
+    pick_up_spot: "请选择自提点",
+    // trimmed_prefectures:null,
+    // prefecture_columns: null,
+    // name_columns: null,
+    tracking_number: "",
+    tracking_numbers: [],
+    phone: "",
+    state: 0, //0:初始 1:添加成功 2：添加失败
+    official_account_url: "",
+    type_index: 0,
+  },
+  tag_clicked(e) {
+    // console.log(e.currentTarget.dataset.index);
+    this.setData({
+      type_index: e.currentTarget.dataset.index
+    })
+  },
+  getProduct() {
+    wx.showLoading({
+      title: '',
+    });
+    wx.cloud.callFunction({
+      name: 'quickstartFunctions',
+      data: {
+        type: 'getProduct',
+        id: ""
+      }
+    }).then((resp) => {
+      console.log(resp);
       this.setData({
-        pick_up_spot:e.target.dataset.name
-      })
-      this.onClose();
-    },
+        product: resp.result.data,
+        sku_length: resp.result.data.sku.length,
+        subtitle: resp.result.data.text.subtitle.split('&hc').join('\n')
+      });
+      // console.log(this.data);
+      wx.hideLoading();
+    }).catch((e) => {
+      console.log(e);
+      wx.hideLoading();
+    });
+  },
+  setPickUpSpot(e) {
+    this.setData({
+      pick_up_spot: e.target.dataset.name
+    })
+    this.onClose();
+  },
 
-    showPopup() {
-      this.setData({ show: true });
-    },
-    onClose() {
-      this.setData({ show: false });
+  showPopup() {
+    this.setData({ show: true });
+  },
+  onClose() {
+    this.setData({ show: false });
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getProduct();
+    wx.showLoading({
+      title: '',
+    });
+    //this.getDelivery();
+    wx.cloud.callFunction({
+      name: 'quickstartFunctions',
+      data: {
+        type: 'getPickUpSpots'
+      }
+    }).then((resp) => {
+      // console.log(resp.result.data);
+      let output = my_library.prefectures_function();
+      //console.log(output);
+      let prefecture_names = output.prefecture_names;
+      let prefectures = output.prefectures;
+      for (let spot of resp.result.data) {
+        // console.log(spot.address)
+        prefectures[spot.address.prefecture].push(spot)
+      }
+      // console.log(prefectures)
+      this.setData({
+        prefecture_names,
+        prefectures
+      });
+      wx.hideLoading();
+    }).catch((e) => {
+      console.log(e);
+      wx.hideLoading();
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    return {
+      title: this.data.product.text.name,
+      // path: '/pages/index/index',
     }
+  }
 })
